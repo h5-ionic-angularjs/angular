a206d669c2aa95d37331f9fc734dc734d06bbf22
scott <zwb_scott@icloud.com>
2015-09-17 23:01:36 +0800

    commit

--- AngularJS-shizhan-damo/3 core-principle-parse/2 ProviderAndInjectorSource/Basic.js
+++
@@ -1,6 +0,0 @@
-//最简单的例子
-var myModule = angular.module("MyModule", []);
-myModule.controller('MyCtrl', ['$scope',function($scope) {
-        $scope.gameName = "大漠吃豆子"
-    }
-]);
--- AngularJS-shizhan-damo/3 core-principle-parse/2 ProviderAndInjectorSource/Injector2.js
+++
@@ -1,9 +0,0 @@
-var myModule = angular.module("MyModule", []);
-
-var MyCtrl = function(thisIsMyName) {
-    thisIsMyName.gameName = "大漠吃豆子";
-}
-
-MyCtrl.$inject = ['$scope'];
-
-myModule.controller('MyCtrl', MyCtrl);
